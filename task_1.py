238992b34aa61830967832b0e6c3bed267464f6a
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2022-12-21 19:33:52 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,6 @@
 def is_palindrome(text):
-    return False
+    new_text = ""
+    lower_text = text.lower()
+    text_list = lower_text.split()
+    new_text = "".join(text_list)
+    return new_text == new_text[::-1]
