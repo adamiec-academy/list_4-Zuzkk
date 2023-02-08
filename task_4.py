257ea9c3e38513da8426d32130d46c36473148c7
Zuzia <115666479+Zuzkk@users.noreply.github.com>
2023-02-08 17:42:47 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -4,11 +4,10 @@ def info(data):
             print(element, end="")
         print()
 
-        
 def border_map(a, b):
     if a == 1:
-        return [b * ['X']]
+        return [b * 'X']
     elif b == 1:
-        return [a * 'X']
+        return [a * ['X']]
     else:
         return [a * ['X']] + [['X'] + (a - 2) * ['.'] + ['X'] for _ in range(1, b-1)] + [a * ['X']]
